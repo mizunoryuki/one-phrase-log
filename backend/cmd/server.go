eaef13916c01666d7ed0a9923663c8dbce983b9b
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2026-01-29 22:43:40 +0900

    init

---
+++ backend/cmd/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"one-phrase-log/graph"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	// 1. GraphQLのクエリ実行用エンドポイント
+	http.Handle("/query", srv)
+
+	// 2. 開発用のPlayground（ブラウザで操作する画面）
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+
+	// 3. Health Check エンドポイント
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
